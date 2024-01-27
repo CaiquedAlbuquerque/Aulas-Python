38be2df53b61fbf2a6aafe461ad296edb97d68d8
Caique Albuquerque <97921186+CaiquedAlbuquerque@users.noreply.github.com>
2024-01-27 17:10:54 -0300

    Update aula38.py

--- Aulas/aula38.py
+++ Aulas/aula38.py
@@ -9,9 +9,11 @@ qtd_colunas = 5
 
 linha = 1
 while linha <= qtd_linhas:
-    
-    print(linha)
-    
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
     linha += 1
+    
     
 print('Acabou') 
