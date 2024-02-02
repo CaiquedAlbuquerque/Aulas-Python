a6ae08943cb6b274a91e251c6b84b8ade357ebf8
Caique Albuquerque <97921186+CaiquedAlbuquerque@users.noreply.github.com>
2024-02-01 22:18:50 -0300

    Create aula42EX.py

---
+++ Aulas/aula42EX.py
@@ -0,0 +1,22 @@
+frase = 'aaa voooo'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+    
+    if letra_atual == ' ':
+        i+= 1
+        continue
+    
+    qtd_atual = frase.count(letra_atual)
+    
+    if qtd_apareceu_mais_vezes < qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i+= 1
+    
+print(f'A letra que apareceu mais vezes foi o "{letra_apareceu_mais_vezes}" que apareceu {qtd_apareceu_mais_vezes}X')
