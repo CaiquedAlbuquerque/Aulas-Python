d96b97302cd563cc5cf5553087a8f6ed176a41eb
Caique Albuquerque <97921186+CaiquedAlbuquerque@users.noreply.github.com>
2024-02-11 15:56:16 -0300

    Create aula50EX.py

---
+++ Aulas/aula50EX.py
@@ -0,0 +1,16 @@
+"""
+Exercicio
+Exiba os indices da lista
+"""
+lista = ['Maria', 'Helena', 'Caique']
+
+
+# for indice in range(len(lista)):
+#     elemento = lista[indice]
+#     print(f'indice:{indice}, valor: {elemento}')
+
+for indice in range(len(lista)):
+    elemento = lista[indice]
+    print(indice, elemento)
+
+    
