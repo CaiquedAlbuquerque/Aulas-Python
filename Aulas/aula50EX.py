c31a962762665d0963af8037d3e4a8770fe64c79
Caique Albuquerque <97921186+CaiquedAlbuquerque@users.noreply.github.com>
2024-02-12 21:10:19 -0300

    Update aula50EX.py

--- Aulas/aula50EX.py
+++ Aulas/aula50EX.py
@@ -3,14 +3,14 @@ Exercicio
 Exiba os indices da lista
 """
 lista = ['Maria', 'Helena', 'Caique']
-
+indices = range(len(lista))
 
 # for indice in range(len(lista)):
 #     elemento = lista[indice]
-#     print(f'indice:{indice}, valor: {elemento}')
+#     print(indice, elemento)
 
-for indice in range(len(lista)):
-    elemento = lista[indice]
-    print(indice, elemento)
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
+
 
     
